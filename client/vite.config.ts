39bd1bb1bb9a44ba54212f3979e6abb264d2fa55
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2024-05-31 13:33:23 -0600

    Deploy to GitHub Pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
- // Ensure this matches your repository name
+  base: '/ReactTemplate/', // Ensure this matches your repository name
 });
